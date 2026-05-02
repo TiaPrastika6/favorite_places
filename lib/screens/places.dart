648992b18c2b74b69c1a214c15c6e9716c990753
Gek Tia Prastika Dewi <gektiaprastika06@gmail.com>
2026-05-02 15:31:17 +0800

    05

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'add_place.dart';
 import '../widgets/places_list.dart';
-import 'package:flutter/material.dart';
+import '../providers/user_places.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
@@ -24,7 +28,7 @@ class PlacesScreen extends StatelessWidget {
         ],
       ),
       body: PlacesList(
-        places: [],
+        places: userPlaces,
       ),
     );
   }
